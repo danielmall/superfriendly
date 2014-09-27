b3b27419c737abfb1bea1690801ba0c2e922579e
Dan Mall <dan@superfriend.ly>
2014-09-27 11:33:48 -0400

    About and projet content added

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
         <section class="screen screen-techcrunch" id="work-techcrunch">
             
             <h1>TechCrunch</h1>
+            <p class="intro">A responsive redesign for a news organization.</p>
 
         </section><!-- .screen -->
 
@@ -72,6 +73,7 @@
         <section class="screen screen-grantland" id="work-grantland">
             
             <h1>Grantland</h1>
+            <p class="intro">A new home for the fan-favorite sports &amp; pop culture blog.</p>            
 
         </section><!-- .screen -->
 
@@ -79,6 +81,7 @@
         <section class="screen screen-ew" id="work-ew">
             
             <h1>EW</h1>
+            <p class="intro">A mobile-responsive site for this fun, smart, and trusted guide to the entertainment world.</p>
 
         </section><!-- .screen -->
 
@@ -131,6 +134,45 @@
         <section class="screen screen-about" id="about">
             
             <h1>About</h1>
+
+            <p>SuperFriendly is a design studio that defeats apathy and the forces of evil. Based in Philly, we make brave sites and apps for screens of all sizes.</p>
+
+            <p>SuperFriendly was started by Dan Mall, an award-winning design director. He is co-founder of Typedia and The Businessology Show. Dan was formerly a Design Director at Big Spaceship, Interactive Director at Happy Cog, technical editor for A List Apart, and singer/keyboard player for contemporary-Christian band Four24.</p>
+
+            <p>We’d love to talk to you about your next project. Drop us a line at info@superfriend.ly.</p>
+
+            <h2>Working with SuperFriendly</h2>
+
+            <p>We use what we call &ldquo;The SuperFriend Model&rdquo; on all of our projects. Just like directors assemble the best crew and actors for their films, we bring together a dream team of partners to work on your project. It allows us to keep overhead low while making sure we&rsquo;re putting together a team with the right expertise. We&rsquo;re not just limited to who we have on payroll. Want an illustrator with a particular style? Need a Rails developer? Would a 3D animator make your product sing? We're connected with the best folks and can pull together the right group for you.</p>
+
+            <p>Some SuperFriends we&rsquo;qve worked with before:</p>
+
+            <ul>
+                <li><a href="http://mattecook.com/">Matt Cook</a><span class="connector">,</span> Producer</li>
+    			<li><a href="http://kevinmhoffman.com/">Kevin M. Hoffman</a><span class="connector">,</span> Information Architect </li>
+    			<li><a href="http://www.kylesteed.com/">Kyle Steed</a><span class="connector">,</span> Illustrator </li>
+    			<li><a href="http://www.philipzaengle.com/">Phil Zaengle</a><span class="connector">,</span> Developer </li>
+    			<li><a href="https://twitter.com/ivanovitch">Ivan Askwith</a><span class="connector">,</span> Strategist </li>
+    			<li><a href="http://bradfrostweb.com/">Brad Frost</a><span class="connector">,</span> Front-End Designer </li>
+    			<li><a href="https://twitter.com/jenniferbrook">Jennifer Brook</a><span class="connector">,</span> Interaction Designer </li>
+    			<li><a href="http://jonathanstark.com/">Jonathan Stark</a><span class="connector">,</span> JavaScript Developer </li>
+    			<li><a href="https://twitter.com/robertgorell">Robert Gorell</a><span class="connector">,</span> Interaction Designer </li>
+    			<li><a href="http://www.behance.net/scottcook">Scott Cook</a><span class="connector">,</span> Art Director </li>
+    			<li><a href="http://www.radiusmotion.com/">Damian Atkinson</a><span class="connector">,</span> 3D modeler &amp; animator </li>
+    			<li><a href="http://www.zachmcnair.com/">Zach McNair</a><span class="connector">,</span> Designer </li>
+    			<li><a href="https://twitter.com/iamjolly">Robert Jolly</a><span class="connector">,</span> Project Manager </li>
+    			<li><a href="http://danrubin.is/">Dan Rubin</a><span class="connector">,</span> Art Director </li>
+    			<li><a href="http://timkadlec.com/">Tim Kadlec</a><span class="connector">,</span> Developer </li>  
+                <li><a href="http://www.stewardesslollipop.com/">Phil Sierzega</a><span class="connector">,</span> Art Director </li>
+    			<li><a href="http://www.sharkbox.com/">Charlie Whitney</a><span class="connector">,</span> Developer </li>
+    			<li><a href="http://hilaryfenton.com/">Hilary Fenton</a><span class="connector">,</span> Interaction Designer </li>
+                <li><a href="http://jennlukas.com/">Jenn Lukas</a><span class="connector">,</span> Developer</li>
+    			<li><a href="http://www.lifeislottery.com/">Lifeislottery</a><span class="connector">,</span> Branding </li>
+    			<li><a href="http://hellobold.com/">Bold</a><span class="connector">,</span> Development </li>
+    			<li><a href="http://monasterydesign.com/">Monastery</a><span class="connector">,</span> Art Direction &amp; Design </li>
+    			<li><a href="http://globalmoxie.com/">Global Moxie</a><span class="connector">,</span> Mobile Design Strategy </li><li><a href="http://arbitrary.io/">Arbitrary</a><span class="connector">,</span> Development </li>
+    			<li><a href="http://10up.com/">10up</a><span class="connector">,</span> Development </li>
+            </ul>
 
         </section><!-- .screen -->
 
