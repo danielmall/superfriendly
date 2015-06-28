932b42b30b15429d6e0d14259be90976318a8ccc
DANIEL MALL <danielmall@dan.local.home>
2015-06-28 13:52:58 -0400

    Getting rid of IE edge meta

--- -/_inc/meta.php
+++ -/_inc/meta.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <?php /* ?><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><?php */ ?>
     <meta name="description" content="" />
 
     <!-- http://blog.javierusobiaga.com/stop-using-the-viewport-tag-until-you-know-ho -->
