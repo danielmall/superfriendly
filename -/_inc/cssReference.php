3a7aef1e8eb1cc10bda193a55e44448d8170f575
DANIEL MALL <danielmall@dan.local.home>
2015-06-29 07:43:39 -0400

    Small nav good on Safari and Chrome

--- -/_inc/cssReference.php
+++ -/_inc/cssReference.php
@@ -1,2 +1,2 @@
-<style><?php require($_SERVER["DOCUMENT_ROOT"].$basepath."/-/c/critical.min.css"); ?></style>
+<style><?php // require($_SERVER["DOCUMENT_ROOT"].$basepath."/-/c/critical.min.css"); ?></style>
     <!-- <script>document.write('<link rel="stylesheet" href="/-/c/enhanced.css" />');</script> -->
